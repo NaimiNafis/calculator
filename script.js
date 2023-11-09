6d0a8e702f919b61a7accf0eecc67fccaeeffc8f
MeeSup <126528516+MeeSup@users.noreply.github.com>
2023-11-09 09:42:06 +0900

    Update updateDisplay() so that value will append Create pseudocode for instruction 6

--- script.js
+++ script.js
@@ -1,34 +1,83 @@
-const add = function(num1, num2){
-    return num1 + num2;
+const add = function(prevNum, currNum){
+    return prevNum + currNum;
   };
   
-  const subtract = function(num1, num2){
-    return num1 - num2;
-  };
-  
-  const multiply = function(num1, num2){
-    return num1 * num2;
-  };
-  
-  const divide = function(num1, num2){
-    return num1 / num2;
-  };
-  
-  let num1, num2, operator;
-  
-  const operate = function(num1, num2, operator){
+const subtract = function(prevNum, currNum){
+    return prevNum - currNum;
+};
+
+const multiply = function(prevNum, currNum){
+    return prevNum * currNum;
+};
+
+const divide = function(prevNum, currNum){
+    if (currNum === 0) {
+        throw new Error("Division by zero");
+      }
+      return prevNum / currNum;
+};
+
+let prevNum = '';
+let currNum = '';
+let operator;
+let buffer = '0';
+
+function updateDisplay(value){
+    // buffer = value; this means you just replace it entirely, we want to append it so,
+    buffer = buffer === "0" ? value : buffer + value;
+    const screen = document.querySelector('.screen');
+    screen.innerText = buffer;
+}
+
+function operate(operator, num1, num2){
     switch(operator){
-      case add:
+      case 'add':
         return add(num1, num2);
-      case subtract:
+      case 'subtract':
         return subtract(num1, num2);
-      case multiply:
+      case 'multiply':
         return multiply(num1, num2);
-      case divide:
+      case 'divide':
         return divide(num1, num2);
       default:
         throw new Error("Invalid operator");
     }
-  };
-  
-  console.log(operate(20, 5, multiply));
+  }
+
+function buttonClick(){
+    const buttons = document.querySelectorAll('.calc-button');
+    buttons.forEach(button => {
+        button.addEventListener('click', (e) => {
+            updateDisplay(e.target.innerText);
+        })
+    });
+}
+
+buttonClick();
+
+/*
+
+IF button click,
+    update e.target.innerText to initButtonListeners
+
+IN initButtonListeners,
+    IF e.target.innerText = value
+        is NaN THEN
+            pass it to isOperator(value)
+        is Number THEN
+            pass it to isNumber(value)
+        THEN operate;
+
+IN isNumber(value),
+    store value to currNum,
+    IF prevNum is still '',
+        prevNum = currNum;
+    ELSE
+        return currNum;
+
+IN operate,
+    let result;
+    FOR EACH cases,
+        DO task and store in result
+        updateDisplay(result);
+*/
