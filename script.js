c04dcf9eb3315fb0343643f03f6e8a3ad30161d8
MeeSup <126528516+MeeSup@users.noreply.github.com>
2023-11-10 23:12:52 +0900

    Copy and merge from 'logic' branch Delete 'logic' branch

--- script.js
+++ script.js
@@ -74,8 +74,7 @@ function clearCount(){
 function handleNumber(value){
     if (value === '.' && buffer.includes('.')) {
         return;
-    }
-    if (operator && buffer === '') {
+    } else if (operator && buffer === '') {
         buffer = value;
     } else {
         buffer = buffer === '0' ? value : buffer + value;
@@ -117,58 +116,3 @@ function clickButton(){
 }
 
 clickButton();
-
-
-/*
-
-// Pseudocode for Calculator Functionality
-
-// When a button is clicked or clickButton()
-IF a button is clicked
-    CALL handleButtonClick with e.target.innerText
-
-// Defining handleButtonClick function
-FUNCTION handleButtonClick(value)
-    IF value is not a number (isNaN(value))
-        CALL handleOperator with value
-    ELSE
-        CALL handleNumber with value
-
-// Defining handleNumber function to handle number input
-FUNCTION handleNumber(value)
-    APPEND value to buffer
-    IF no previous operation pending (operator is undefined)
-        SET prevNum to buffer value
-    UPDATE display with buffer
-
-// Defining handleOperator function to handle operator input
-FUNCTION handleOperator(value)
-    IF value is '='
-        IF prevNum and operator are defined
-            CALL operate with prevNum, buffer (as currNum), and operator
-        ELSE
-            DO nothing (no operation to complete)
-    ELSE IF value is one of the operators '+', '-', '*', '/'
-        IF prevNum is not empty and operator is defined
-            CALL operate with prevNum, buffer (as currNum), and operator
-            RESET buffer to empty string
-            RESET prevNum to result of operation
-        SET operator to value
-    UPDATE display if needed
-
-// Defining operate function to perform the calculation
-FUNCTION operate(prevNum, currNum, operator)
-    SWITCH operator
-        CASE '+'
-            COMPUTE prevNum + currNum
-        CASE '-'
-            COMPUTE prevNum - currNum
-        CASE '*'
-            COMPUTE prevNum * currNum
-        CASE '/'
-            COMPUTE prevNum / currNum
-    STORE the result
-    UPDATE display with result
-    RESET buffer, prevNum, and operator as needed for next calculation
-
-*/
